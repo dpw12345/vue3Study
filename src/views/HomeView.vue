5429bc22a9d568b6b64ffbe2ba8a25f259d5c46d
dingpeiwen <1529429155@qq.com>
2023-07-20 22:41:57 +0800

    refactor:项目初始化

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+
+<div>{{ counter.count }}</div>
+<div>
+    <button @click="add">加</button>
+</div>
+<div>{{ counter.doubleCount }}</div>
+
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive } from 'vue' 
+import { useCounterStore } from '@/stores/counter';
+const counter = useCounterStore()
+const add = ()=>{
+    counter.increment()
+}
+
+</script>
+<style scoped>
+
+</style>
